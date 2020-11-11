b9a778799f678f48f36baffdb6b7db09292ce8db
Bummelnderboris <Bummelnderboris@users.noreply.github.com>
2020-11-11 11:02:33 +0100

    1

---
+++ workspace/snakerinoFX/Snake/src/Game/Config.java
@@ -0,0 +1,13 @@
+package Game;
+
+public class Config {
+    // Board configuration
+    // These are constants of the game
+    // maybe possible to use as XML to make configurable game values
+    public final int width = 800;
+    public final int height = width;
+    public final int rows = 10;
+    public final int columns = 10;
+    public final int square_size = width / rows;
+
+}
